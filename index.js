82ff8de9b247012026a77745a52fd069de61d6a7
Brooke Morrison <bwookieeeee@gmail.com>
2021-05-30 19:22:50 -0700

    Added ability to revoke invites if the user is the broadcaster or a mod

--- index.js
+++ index.js
@@ -13,42 +13,73 @@ const twitch = new tmi.client({
 let inviteChannel;
 const client = new Discord.Client();
 
-
 twitch.on("message", onMessageHandler);
 twitch.on("connected", onConnectedHandler);
 
 // Wait for discord to ready up before connecting to twitch.
 client.on("ready", () => {
   twitch.connect();
-})
+});
 
 async function onMessageHandler(target, context, msg, self) {
   if (self) return;
 
-  const command = msg.trim();
+  if (msg.startsWith("!")) {
+    const cmd = msg.trim().substring(1);
 
-  if (command === "!discord") {
-    try {
-
-      const link = await inviteChannel.createInvite({
-        maxAge: 86400,
-        maxUses: 1,
-        unique: true,
-        reason: `Generated from twitch via ${context.username}`
-      })
-      twitch.say(target, `Here's your discord link! It only has one use and expires after 24 hours. ${link.url}`)
-      console.log(`Generated invite ${link.code} for ${context.username}`);
-    } catch (e) {
-      console.error(e);
-      twitch.say(target, "Something went wrong processing your request. Check error logs for more info.")
+    switch (cmd) {
+      case "discord":
+        await generateInvite(target, context);
+        break;
+      case "revoke":
+        await revokeInvite(target, context);
+        break;
     }
   }
-
 }
 
+async function generateInvite(target, context) {
+  try {
+    const link = await inviteChannel.createInvite({
+      maxAge: 86400,
+      maxUses: 1,
+      reason: `Generated from Twitch via ${context.username}`
+    });
+    twitch.say(
+      target,
+      `Here's your Discord link! It only has one use and expires after 24 hours. ${link.url}`
+    );
+    console.log(`Generated invite ${link.code} for ${context.username}`);
+  } catch (e) {
+    console.error(e);
+    twitch.say(
+      target,
+      "Something went wrong generating an invite. Check error logs for more info."
+    );
+  }
+}
+
+async function revokeInvite(target, context) {
+  if (context.mod || context["user-id"] === context["room-id"]) {
+    try {
+      inviteChannel.fetchInvites().then((invites) => {
+        const key = invites.firstKey();
+        const invite = invites.get(key);
+        invite.delete(`Revoked by ${context.username}`);
+        console.log(`Deleted invite ${key} by ${context.username}`);
+        twitch.say(target, `Revoked invite ${key}`);
+      });
+    } catch (e) {
+      console.error(e);
+      twitch.say("Could not revoke invite, check logs for more info.");
+    }
+  } else {
+    twitch.say(target, "You are authorized to revoke invites.");
+  }
+}
 
 async function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`)
+  console.log(`* Connected to ${addr}:${port}`);
   inviteChannel = await client.channels.fetch(settings.discord.targetChannel);
 }
 
